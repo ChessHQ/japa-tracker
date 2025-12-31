7be3aac2cfe1ebc97695945a6f0c20fcff5a868a
Arjun Tiwari <atiw88@outlook.com>
2025-12-30 18:46:55 -0800

    added button to start a new round, prettier

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,44 +1,63 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { Image } from "expo-image";
+import { Platform, StyleSheet } from "react-native";
 
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
-import { useEffect, useState } from 'react';
+import ParallaxScrollView from "@/components/parallax-scroll-view";
+import { ThemedText } from "@/components/themed-text";
+import { ThemedView } from "@/components/themed-view";
+import { Link } from "expo-router";
+import { useEffect, useState } from "react";
 
 export default function HomeScreen() {
   // useState and useEffect
 
   // state:
   // const [variable, function-that-sets-variable-when-u-want-to-update-it] = useState<type>(default-value)
-  const [currentBead, setCurrentBead] = useState<number>(0) // react state hooks
-  const [roundCompleted, setRoundCompleted] = useState<boolean>(false)
+  const [currentBead, setCurrentBead] = useState<number>(0); // react state hooks
+  const [roundCompleted, setRoundCompleted] = useState<boolean>(false);
+  const [currentRound, setCurrentRound] = useState<number>(0);
 
   const handleBeadClick = () => {
-    setCurrentBead(currentBead + 1)
-  }
+    setCurrentBead(currentBead + 1);
+  };
+
+  const handleReset = () => {
+    setCurrentBead(0);
+    setRoundCompleted(false);
+  };
 
   useEffect(() => {
     if (currentBead === 8) {
-      setRoundCompleted(true)
+      setRoundCompleted(true);
+      setCurrentRound((prev) => prev + 1);
     }
-  }, [currentBead, /*other dependency vars*/])
+  }, [currentBead /*other dependency vars*/]);
 
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#0a8031ff', dark: '#0a8031ff' }}
-      headerImage={
-        <Image></Image>
-      }
-      >
+      headerBackgroundColor={{ light: "#0a8031ff", dark: "#0a8031ff" }}
+      headerImage={<Image></Image>}
+    >
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Start chanting your rounds!</ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Japa Counter</ThemedText>
         <ThemedText type="default">Current Bead: {currentBead}</ThemedText>
-        <button disabled = {roundCompleted} style={styles.button} onClick={handleBeadClick}>Chant</button>
+        <ThemedText type="default">Current Round: {currentRound}</ThemedText>
+        <button
+          disabled={roundCompleted}
+          style={styles.button}
+          onClick={handleBeadClick}
+        >
+          Chant
+        </button>
+        <button
+          disabled={!roundCompleted}
+          style={styles.button}
+          onClick={handleReset}
+        >
+          Start a new round
+        </button>
       </ThemedView>
     </ParallaxScrollView>
   );
@@ -50,8 +69,8 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 8,
   },
   stepContainer: {
@@ -63,12 +82,12 @@ const styles = StyleSheet.create({
     width: 290,
     bottom: 0,
     left: 0,
-    position: 'absolute',
+    position: "absolute",
   },
   button: {
     height: 50,
     width: 250,
     bottom: -300,
     left: 0,
-  }
+  },
 });
